51d89c37731c718bb3129c43523eee01b92a1f8d
Craig <indigomx9@gmail.com>
2023-02-12 12:58:37 -0500

    I Updated the Film component with the correct URL.

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ITrending } from "../models/Interfaces";
-const imgURL = "https://image.tmbd.org/t/p/w500/"
+const imageUrl = "https://image.tmdb.org/t/p/w500"
 
 type FILM = {
     film: ITrending
@@ -10,6 +10,10 @@ export const Film = ({film}: FILM) => {
     return (
         <React.Fragment>
             <h1>{film.title}</h1>
+            <img 
+                alt={film.title} 
+                src={`${imageUrl}/${film.backdrop_path}`} 
+            />
         </React.Fragment>
     );
 };
